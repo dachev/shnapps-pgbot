ea976f0ec49aa1d10733318a56ed54d07239d073
Blagovest Dachev <blago@dachev.com>
2013-05-08 19:47:56 -0500

    Code style changes

--- bin/import.js
+++ bin/import.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-var Fs   = require('fs');
-var Path = require('path');
+var fs   = require('fs');
+var path = require('path');
 var _    = require('underscore');
 
 var host = '127.0.0.1';
@@ -9,40 +9,40 @@ var port = 8080;
 var core = 'pgbot';
 var solr = require('../modules/solr').createClient(host, port, core);
 
-var name  = Path.join(__dirname, '../data/sentences.txt');
-Fs.readFile(name, 'utf8', function(err, data) {
+var name  = path.join(__dirname, '../data/sentences.txt');
+fs.readFile(name, 'utf8', function(err, data) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  
+  var sentences = data.split('\n');
+  var documents = _.reduce(sentences, function(memo, item, key) {
+    memo.push({
+      id   : (key+1).toString(),
+      text : item
+    });
+    
+    return memo;
+  }, []);
+  
+  solr.del(null, '*:*', function(err, response) {
     if (err) {
-        console.log(err);
-        process.exit(1);
+      solr.destroy();
+      console.log(err);
+      process.exit(1);
     }
     
-    var sentences = data.split('\n');
-    var documents = _.reduce(sentences, function(memo, item, key) {
-        memo.push({
-            id   : (key+1).toString(),
-            text : item
-        });
-        
-        return memo;
-    }, []);
-    
-    solr.del(null, '*:*', function(err, response) {
-        if (err) {
-            solr.destroy();
-            console.log(err);
-            process.exit(1);
-        }
-        
-        solr.add(documents, function(err, response) {
-            if (err) {
-                solr.destroy();
-                console.log(err);
-                process.exit(1);
-            }
-            
-            solr.commit();
-            solr.optimize();
-            solr.destroy();
-        });
+    solr.add(documents, function(err, response) {
+      if (err) {
+        solr.destroy();
+        console.log(err);
+        process.exit(1);
+      }
+      
+      solr.commit();
+      solr.optimize();
+      solr.destroy();
     });
+  });
 });
